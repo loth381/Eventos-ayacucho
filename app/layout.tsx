21c11258e4971f9e19cee03a40a0920c3203feba
Henri <u19217724@gmail.com>
2024-04-20 17:57:00 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins =Poppins ({
+  subsets:["latin"],
+  weight:["400", "500", "600", "700" ],
+  variable:"--font-poppins",
+})
+
+
+export const metadata: Metadata = {
+  title: "Eventos",
+  description: "plataforma de eventos en ayacucho",
+  icons:{
+    icon:"/assets/images/logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
