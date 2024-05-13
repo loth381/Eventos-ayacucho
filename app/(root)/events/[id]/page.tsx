0da70cba2092dfb200a9c070132f652e53582e10
Henri <u19217724@gmail.com>
2024-05-13 16:58:48 -0500

    webhook de stripe

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,10 +1,18 @@
-import { getEventById } from "@/lib/actions/event.actions";
+import CheckoutButton from "@/components/shared/CheckoutButton";
+import Collection from "@/components/shared/Collection";
+import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 
-const EventDetail = async ({ params: { id } }: SearchParamProps) => {
+const EventDetail = async ({ params: { id }, searchParams }: SearchParamProps) => {
     const event = await getEventById(id);
+
+    const relatedEvents =   await getRelatedEventsByCategory({
+        categoryId:event.category._id,
+        eventId:event._id,
+        page: searchParams.page as string,
+    })
 
     return (
         <>
@@ -39,6 +47,10 @@ const EventDetail = async ({ params: { id } }: SearchParamProps) => {
                                 </p>
                             </div>
                         </div>
+
+
+                        <CheckoutButton event={event} />
+
 
                         <div className="flex flex-col gap-5">
                             <div className="flex gap-2 md:gap-3">
@@ -79,6 +91,22 @@ const EventDetail = async ({ params: { id } }: SearchParamProps) => {
                         </div>
                     </div>
                 </div>
+            </section>
+
+
+            {/* EVENTS with the same category */}
+
+            <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+                <h2 className="h2-bold">Related Events</h2>
+                <Collection
+                    data={relatedEvents?.data}
+                    emptyTitle="No Events Found"
+                    emptyStateSubtext="Come back later"
+                    collectionType="All_Events"
+                    limit={3}
+                    page={searchParams.page as string}
+                    totalPages={relatedEvents?.totalPages}
+                />
             </section>
         </>
     );
